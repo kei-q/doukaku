b2d21d29981c067ad8d36e0b4d316c18d9705024
kei_q <keqh.keqh@gmail.com>
2014-10-04 18:48:52 +0900

    隣り合う要素を計算するように

--- answers/26/Solver.hs
+++ answers/26/Solver.hs
@@ -1,42 +1,41 @@
-{-# LANGUAGE TupleSections #-}
 module Solver where
-
--- imports
 
 import Data.List
 import Data.List.Split
-import qualified Doukaku as D
-import Data.Maybe
-import Data.Tuple
-
--- ghci用の値
 
 t = "bdelmnouy"
 
--- 定型句
-
-type Input = [String]
-type Output = (Int,Int,Int)
+type Input = String
+type Output = [Int]
 
 solve :: String -> String
 solve = format . solve'
 
 format :: Output -> String
-format (r,g,b) = intercalate "," [show r, show g, show b]
+format ls = intercalate "," $ map show ls
 
 -- main
 
-solve' :: String -> (Int,Int,Int)
-solve' xs = let (r,g,b) = neighbor xs in (base-r*2,base-g*2,base-b*2)
-  where
-    base = length xs
+solve' :: Input -> Output
+solve' xs = map (\n -> length xs - n*2) $ neighbors xs
 
-neighbor :: String -> (Int,Int,Int)
-neighbor xs = (count xs rlines, count xs glines, count xs blines)
+neighbors :: Input -> [Int]
+neighbors xs = map (count xs) [rlines, glines, blines]
 
 count :: String -> [String] -> Int
-count xs ls = length $ filter (\p -> length (xs `intersect` p) == 2) ls
+count xs = length . filter (\p -> length (xs `intersect` p) == 2)
 
-rlines = ["bc","ef","gh","jk","lm","no","qr","st","uv","wx"]
-glines = ["cd", "fg", "hi", "kl", "mn", "op", "rs", "tu", "vw", "xy"]
-blines = ["ac", "bf", "dh", "ek", "gm", "io", "jr", "lt", "nv", "px"]
+makeField :: Int -> [(Int,Int,Int,Int)]
+makeField limit = takeWhile (\(c,_,_,_) -> c<=limit) $ go 1 2
+  where
+    go count n = map three (take count [n,n+2..]) ++ go (count+1) (n+count*2+1)
+      where
+        three n = (n, n-1, n-count*2, n+1)
+
+roots = makeField 25
+rlines = map (\(c,r,_,_) -> map n2c [r,c]) roots
+blines = map (\(c,_,b,_) -> map n2c [b,c]) roots
+glines = map (\(c,_,_,g) -> map n2c [c,g]) roots
+
+n2c :: Int -> Char
+n2c n = toEnum $ fromEnum 'a' + n
