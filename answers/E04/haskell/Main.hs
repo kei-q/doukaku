1169cedfc05cc3b1fef344598134a61e37ded972
keqh <keqh.keqh@gmail.com>
2016-06-04 19:37:19 +0900

    Cosmetic

--- answers/E04/haskell/Main.hs
+++ answers/E04/haskell/Main.hs
@@ -4,59 +4,49 @@ module Main where
 
 import Data.List
 import Data.List.Split
-import Control.Lens
-import Numeric.Lens
-import Debug.Trace
 import Data.Char
 
 main :: IO ()
 main = undefined
 
-t :: Show a => a -> b -> b
-t = traceShow
-ti :: Show a => a -> a
-ti = traceShowId
-(.!) g f x = g (ti (f x))
-
-type Input = ([(Int,Int)],Char)
+type Input = (Routes,Char)
 type Output = String
 
 solve :: String -> String
-solve = format . solve' . parse
+solve = solve' . parse
 
 parse :: String -> Input
-parse source = (routes,head c)
+parse source = (routes, head c)
   where
     -- oops!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
     [rs,c] = splitOn ":" source
     routes = zip (map (\c -> (read [c] :: Int) - 1) rs) [0..]
 
--- Nothingや[]のケースはここでformatする
-format :: Output -> String
-format = id
-
 -- main
 
 type Routes = [(Int,Int)]
 
-solve' :: Input -> Output
-solve' input = map fst $ filter (not . snd) $ zip ['A'..] $ clumb input
+climbers = ['A'..'H']
 
-clumb (rs,c)
-  | block == [] = map (==c) ['A'..'H']
-  | otherwise = map (\r -> (r `intersect` block) /= []) (routes rs)
+solve' :: Input -> Output
+solve' = map fst . filter (not . snd) . zip climbers . climb
+
+climb :: (Routes, Char) -> [Bool]
+climb (rs,c)
+  | block == [] = map (==c) climbers
+  | otherwise = map ((/=[]) . intersect block) (routes rs)
   where
     block = route rs a
     a = ord c - ord 'A'
 
-routes rs = map (\(c,p) -> route (reverse rs) p) clumber
-clumber = zip ['A'..'H'] [0..]
+routes :: Routes -> [Routes]
+routes rs = map (route (reverse rs)) [0..7]
 
 route :: Routes -> Int -> Routes
 route [] _ = []
-route ((e,i):rs) n
-  | e == n      = (e,i) : route rs (next n)
-  | e == prev n = (e,i) : route rs (prev n)
+route (r@(e,_):rs) n
+  | e == n      = r : route rs (next n)
+  | e == prev n = r : route rs (prev n)
   | otherwise = route rs n
 
 prev n = (n+7) `mod` 8
